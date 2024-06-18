85474938dae4b2c6723104f8568727e29ebf57f5
Quan Dao Anh <anhquan7303qqq@gmail.com>
2024-06-18 16:15:10 +0700

    Initial commit

---
+++ src/main/java/com/example/VNPT_Project/VnptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.VNPT_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnptProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnptProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VNPT_Project/VnptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VNPT_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
