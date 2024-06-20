e0c62530b2fea18460ae8fa4db4fb463d298dabb
Quan Dao Anh <anhquan7303qqq@gmail.com>
2024-06-20 16:13:16 +0700

    update

---
+++ src/test/java/com/backend/VNPT_Intern_Project/VnptInternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.VNPT_Intern_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnptInternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
